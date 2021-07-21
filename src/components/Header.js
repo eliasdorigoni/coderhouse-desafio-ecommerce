085c8e0cdd2a264d4f56b3633dab07e92df8178c
Elias Dorigoni <edorigoni@lavozdelinterior.com.ar>
2021-07-21 10:29:58 -0300

    Agrega componentes header y navbar

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+const Header = () =>
+    <header>
+        <h1>Titulo</h1>
+    </header>
+
+export default Header
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+const Navbar = () =>
+    <nav>
+        <a>Menu 1</a>
+        <a>Menu 2</a>
+        <a>Menu 3</a>
+    </nav>
+
+export default Navbar
