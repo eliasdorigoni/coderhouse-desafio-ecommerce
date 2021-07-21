3bc47304eecff01505c6e3120cde36e3d5a0a669
Elias Dorigoni <edorigoni@lavozdelinterior.com.ar>
2021-07-21 13:31:16 -0300

    Elimina wrapper innecesario

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,11 @@
 import CartWidget from './CartWidget'
 
 const Navbar = () =>
-    <>
-        <nav>
-            <p className="item">Menu 1</p>
-            <p className="item">Menu 2</p>
-            <p className="item">Menu 3</p>
-            <CartWidget />
-        </nav>
-    </>
+    <nav>
+        <p className="item">Menu 1</p>
+        <p className="item">Menu 2</p>
+        <p className="item">Menu 3</p>
+        <CartWidget />
+    </nav>
 
 export default Navbar
