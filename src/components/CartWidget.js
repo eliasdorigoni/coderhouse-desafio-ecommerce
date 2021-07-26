1e21294a1c7ded858468e475467badfa16c9ee4e
Elias Dorigoni <edorigoni@lavozdelinterior.com.ar>
2021-07-26 10:23:10 -0300

    Fix url carrito

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,4 +1,3 @@
-
 const CartWidget = () =>
     <div className="cartWidget text-right">
         <div className="text-xs pr-2 pt-1">
@@ -7,7 +6,7 @@ const CartWidget = () =>
             </span>
             <div className="relative inline-block">
                 <img className="inline-block w-6 h-6"
-                    alt="Carrito" src="/svg/cart.svg" />
+                    alt="Carrito" src="/coderhouse-desafio-ecommerce/svg/cart.svg" />
                 <span className="bg-red-500 text-white text-xs rounded-full
                     flex items-center justify-center h-4 w-4 absolute -top-1 -right-2"
                     >6</span>
