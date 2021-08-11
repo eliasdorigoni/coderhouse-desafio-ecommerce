bde75370891fb4fec7eec7e7a0bb9e9f6a57b94c
Elias Dorigoni <eliasdorigoni@gmail.com>
2021-08-10 21:33:59 -0300

    Elimina items que no voy a maquetar.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,14 +12,6 @@ const Footer = () =>
             ventas@mikesmechs.com
         </p>
 
-        <nav className="mb-8">
-            <p className="inline hover:underline cursor-pointer">Sucursales</p> | <p
-            className="inline hover:underline cursor-pointer">Empleos</p> | <p
-            className="inline hover:underline cursor-pointer">Devoluciones y reclamos</p> | <p
-            className="inline hover:underline cursor-pointer">Legales</p> | <p
-            className="inline hover:underline cursor-pointer">Políticas de privacidad</p>
-        </nav>
-
         <p className="border-t-2 border-main-light pt-8 pb-4 mx-12">
             Ecommerce diseñado y desarrollado por <span className="text-yellow-600 font-bold">Elías Dorigoni</span> para el curso de React JS de <span className="text-green-600 font-bold">Coderhouse</span>.
             <br />
